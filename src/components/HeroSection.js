d939dd33fa96148f7a9379972ebfcb03c48c7d0c
kiingdavies <daviesadedeji007@gmail.com>
2021-07-12 18:26:48 +0100

    CI test

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -14,7 +14,7 @@ function HeroSection() {
             
             Directory
       </h1>
-      <p>Find your favorite Characters, Films, Species,</p>
+      <p>SFind your favorite Characters, Films, Species,</p>
       <p>Starships and Planets</p>
       {/* <div className='hero-btns'>
         <Button
